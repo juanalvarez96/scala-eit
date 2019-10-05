1724d622a07c484e23c8bf1452b413db267cbef9
Juan Álvarez Fernández del Vallado <juan@Juans-MacBook-Pro.local>
2019-10-05 14:06:40 +0200

    Initial commit

---
+++ src/computeExpression.scala
@@ -0,0 +1,20 @@
+import java.beans.Expression
+
+abstract class Expression {
+  case object  X extends Expression
+  case class Const(value: Int) extends Expression
+  case class Add(left: Expression, right: Expression) extends Expression
+  case class Mult(left: Expression, right: Expression) extends Expression
+  case class Neg(expr: Expression) extends Expression
+
+  def eval(expression: Expression, xValue : Int):Int =
+    expression match{
+      case X=> xValue
+      case Const(value)=> value;
+      case Add(left, right)=> eval(left, xValue) + eval(right, xValue)
+      case Mult(left, right)=> eval(left, xValue) * eval(right, xValue)
+      case Neg(expr)=> - eval(expr, xValue)
+    }
+
+
+}
